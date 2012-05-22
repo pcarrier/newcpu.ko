1f9ccf5c697841dda95931f39cca70aed9b27d12
Pierre Carrier <pierre@spotify.com>
2012-05-22 19:38:44 +0200

    add README, cleanups

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 obj-m += newcpu.o
 
 all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C /lib/modules/3.3.7-1-ck/build M=$(PWD) modules
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- newcpu.c
+++ newcpu.c
@@ -18,21 +18,19 @@ static char oldname[NEWCPU_MODEL_ID_LENGTH];
 static char *newname = NEWCPU_DEFAULT_NAME;
 
 module_param_named(name, newname, charp, S_IRUGO);
-MODULE_PARM_DESC(name, "CPU name (defaults to " NEWCPU_DEFAULT_NAME ")");
+MODULE_PARM_DESC(name, "New CPU name (defaults to " NEWCPU_DEFAULT_NAME ")");
 
 int init_module()
 {
-    size_t i, name_length = strlen(newname);
+    size_t i;
 
-    if (name_length >= NEWCPU_MODEL_ID_LENGTH) {
+    if (strlen(newname) >= NEWCPU_MODEL_ID_LENGTH)
         return (-1);
-    }
 
     strcpy(oldname, (&cpu_data(0))->x86_model_id);
 
-    for (i = 0; i < nr_cpu_ids; i++) {
+    for (i = 0; i < nr_cpu_ids; i++)
         strcpy((&cpu_data(i))->x86_model_id, newname);
-    }
 
     printk("%s gone, giving room to %s!\n", oldname, newname);
 
@@ -41,10 +39,10 @@ int init_module()
 
 void cleanup_module()
 {
-    loff_t i;
-    for (i = 0; i < nr_cpu_ids; i++) {
+    size_t i;
+
+    for (i = 0; i < nr_cpu_ids; i++)
         strcpy((&cpu_data(i))->x86_model_id, oldname);
-    }
 
     printk("%s gone, %s back.\n", newname, oldname);
 }
